29c5140ea8cf555c184ac51775f975206663331b
nemo-omen <nemo.omen@gmail.com>
2022-04-15 11:54:45 -0500

    Abandon!

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+const feedUrls = [
+   'https://www.sarasoueidan.com/blog/index.xml'
+];
+
+const getFeeds = function () {
+   feedUrls.forEach(async function (url) {
+      const rssResponse = await fetch(url);
+      const rssData = await rssResponse.text();
+      console.log(rssData);
+   });
+};
+
 document.addEventListener('DOMContentLoaded', function () {
 // modules use strict mode by default, so this is just for
 // JSLint's benefit
    'use strict';
+   getFeeds();
 });
